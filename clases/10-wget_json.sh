86d35c3fef16ffe5bb01849ef19640fe8a171b09
fereste <fernando.piubel@gmail.com>
2024-04-16 20:02:05 -0300

    Agrego ejemplo de wget + jq

--- clases/10-wget_json.sh
+++ clases/10-wget_json.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Demonios
 
-function manejador() {
-    echo "acabo de recibir un SIGTERM"
-}
+# Usando "wget" para realizar una petición a una API REST.
+# Usamos de ejemplo la API de "jsonplaceholder" que nos devuelve un JSON con los datos de un usuario.
+# * El parámetro O- indica que la salida sea por stdout. Eliminando este parámetro se puede guardar la salida en un archivo.
+# * El parámetro q indica que no muestre mensajes de progreso.
+wget -qO- https://jsonplaceholder.typicode.com/users/1
 
-trap manejador SIGTERM
-trap - SIGTERM
-trap -p
+# Para interpretar el JSON se puede usar "jq".
+# * Se puede instalar con "sudo apt install jq" ya que no viene por defecto en la instalación de Ubuntu.
+# * Se puede usar para extraer datos de un JSON.
+wget -qO- https://jsonplaceholder.typicode.com/users/1 | jq '.name'
+wget -qO- https://jsonplaceholder.typicode.com/users/1 | jq '.address.street'
+wget -qO- https://jsonplaceholder.typicode.com/users/1 | jq '{nombre:.name, calle:.address.street}'
 
-sleep 5
-
-while [ 1 -eq 1 ]
-do
-    date
-    sleep 1
-done
+# Generando un JSON con "jq".
+# * Se puede usar para generar un JSON.
+jq -n '{nombre:"Juan", edad:30}'
